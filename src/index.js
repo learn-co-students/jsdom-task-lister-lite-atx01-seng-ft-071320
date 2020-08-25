ebc90c92a867f3baffe7cdfb25123111ee755137
jtskidmore <johntoddskidmore@gmail.com>
2020-08-25 11:11:52 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.getElementById("form-submit").addEventListener("click", function(event){
+    event.preventDefault();
+    addListItem();
+  });
 });
+
+
+
+function addListItem() {
+  const inputValue = document.getElementById("new-task-description").value;
+  const ul = document.getElementById('tasks');
+  const li = document.createElement('li');
+  li.innerHTML = inputValue;
+  ul.appendChild(li);
+}
