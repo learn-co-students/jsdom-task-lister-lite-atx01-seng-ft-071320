b78f027dd6ac70502b1b1edc2974ae6619172e7a
Gabe-Corral <78gabe.corral@gmail.com>
2020-08-25 11:09:56 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+});
+
+const form = document.querySelector('form');
+const list = document.querySelector('ul');
+
+form.addEventListener('submit', function(event) {
+  //let input = document.getElementById("new-task-description").value;
+  const task = document.createElement("li");
+  const button = document.createElement("BUTTON");
+
+  task.innerHTML = `${form.elements[0].value}`;
+  list.appendChild(task);
+  button.innerHTML = "x";
+  task.appendChild(button);
+  button.addEventListener("click", function(event) {
+    task.remove();
+  });
+  event.preventDefault();
 });
