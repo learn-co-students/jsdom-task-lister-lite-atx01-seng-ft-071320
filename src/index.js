afda97508cb8d16602466d1eed51ab26042e52d2
lgonzalez2 <lgonzalez_94@hotmail.com>
2020-08-25 14:33:25 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const form = document.getElementById('create-task-form');
+  form.addEventListener('submit', handleSubmit);
 });
+
+
+function handleSubmit(e) {
+  e.preventDefault();
+  addTask(e.target['new-task-description'].value); //check the arg in console.log first!
+  document.getElementById('new-task-description').value = '';
+}
+
+function addTask(task) {
+  const list = document.getElementById('tasks');
+
+  const item = document.createElement('li');
+  item.innerText = task;
+
+  // const deleteButton = document.createElement('button');
+  // deleteButton.innerText = '[X]';
+  // deleteButton.addEventListener('click', () => item.remove());
+
+
+  // item.append(deleteButton);
+  list.append(item);
+}
+
